57edb0510c463b841f3b182a57c1be84bac198ac
Alex Santisteban <alex.santisteban@canonical.com>
2025-06-13 18:52:31 +0200

    fix: upgrade setup

--- advanced_level/setup.sh
+++ advanced_level/setup.sh
@@ -1,11 +1,3 @@
 cd advanced_level
-apt update
-DEBIAN_FRONTEND=noninteractive apt install -y snapd || true
-# Ignore errors here because snapd may fail to start services properly
-
-# Wait for snapd.socket to be ready or skip installing rockcraft snap if not critical
-sleep 5
-
-snap install rockcraft --classic || echo "snap install failed, skipping"
-
-lxd init --auto || echo "lxd init failed, skipping"
+lxd init --auto
+apt update && apt install -y snapd
