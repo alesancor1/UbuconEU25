b800ac9cc05fa2897949042780485de70efd19b4
Alex Santisteban <alex.santisteban@canonical.com>
2025-06-13 19:04:31 +0200

    fix: update setup

--- advanced_level/setup.sh
+++ advanced_level/setup.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
+PID_FILE="/tmp/snapd-install.pid"
+
 cd advanced_level
 
 echo installing snapd...
 apt update && apt install -y snapd &
-echo $! > /tmp/snapd-install.pid
+echo $! > $PID_FILE
 
 PID=$(cat "$PID_FILE")
 while kill -0 "$PID" 2>/dev/null; do
