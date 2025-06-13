d554e8de07f2125e2ebd2b8fce4da82cf7f448ac
Alex Santisteban <alex.santisteban@canonical.com>
2025-06-13 18:28:22 +0200

    fix: simplify script

--- advanced_level/setup.sh
+++ advanced_level/setup.sh
@@ -1,22 +1,4 @@
-echo "Setting up the scenario..."
-echo "Installing snapd..."
-
 apt update
 apt install snapd
-
-clear
-
-echo "Setting up the scenario..."
-echo "Installing rockcraft snap..."
 snap install rockcraft --classic
-
-clear
-
-echo "Seting up lxd..."
 lxd init --auto
-
-echo "Done!"
-
-sleep 3
-
-clear
