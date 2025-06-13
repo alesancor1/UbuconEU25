9f438f4a5197d1b2b3d5960d380e0b0ee1dc94a6
Alex Santisteban <alex.santisteban@canonical.com>
2025-06-13 19:19:04 +0200

    fix: update setup

--- advanced_level/setup/background.sh
+++ advanced_level/setup/background.sh
@@ -2,4 +2,7 @@
 
 apt update 
 apt install -y snapd
+
+sleep 5
+
 echo done > /tmp/background0
--- advanced_level/setup/foreground.sh
+++ advanced_level/setup/foreground.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+
+cd advanced_level
 echo setting up snapd
 while [ ! -f /tmp/background0 ]; do sleep 1; done
 
 snap install rockcraft --classic
 lxd init --auto
-
-cd advanced_level
