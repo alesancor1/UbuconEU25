e8dfa08fd909ec20093e2706317cf6db52ca5d35
Alex Santisteban <alex.santisteban@canonical.com>
2025-06-13 19:10:09 +0200

    fix: update script

--- advanced_level/setup.sh
+++ advanced_level/setup.sh
@@ -5,13 +5,6 @@ PID_FILE="/tmp/snapd-install.pid"
 cd advanced_level
 
 echo installing snapd...
-(apt update && apt install -y snapd) &
+apt update 
+apt install -y snapd &
 echo $! > $PID_FILE
-
-PID=$(cat "$PID_FILE")
-while kill -0 "$PID" 2>/dev/null; do
-    sleep 1
-done
-
-snap install rockcraft classic
-lxd init --auto
