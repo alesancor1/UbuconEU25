338a872eff7c925c6f2ffdb90afcd8542969519d
Alex Santisteban <alex.santisteban@canonical.com>
2025-06-13 18:57:51 +0200

    fix: install snapd in background

--- advanced_level/setup.sh
+++ advanced_level/setup.sh
@@ -1,3 +1,13 @@
 cd advanced_level
+
+echo installing snapd...
+apt update && apt install -y snapd &
+echo $! > /tmp/snapd-install.pid
+
+PID=$(cat "$PID_FILE")
+while kill -0 "$PID" 2>/dev/null; do
+    sleep 1
+done
+
+snap install rockcraft classic
 lxd init --auto
-apt update && apt install -y snapd
